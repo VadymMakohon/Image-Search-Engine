be93d2efb37841f79d18a72a7fa15946e83a337f
VadymMakohon <errorks7@gmail.com>
2024-09-23 19:57:52 +0200

    added more lines to the code

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const serchForm = document.getElementById("search-form");
-const serchBox = document.getElementById("search-box");
-const serchResult = document.getElementById("search-result");
+const accessKey = "18kM1ktlMdk-usIpso39A1L3NaKD60jgloZ_8ll9CzM";
+
+const searchForm = document.getElementById("search-form"); // Correct id
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
 
 let keyword = "";
@@ -8,5 +10,23 @@ let page = 1;
 
 async function searchImages() {
     keyword = searchBox.value;
-    canst url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}';
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+    })
 }
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1; // Reset to page 1 on new search
+    searchImages();
+});
