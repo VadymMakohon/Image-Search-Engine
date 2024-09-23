7c2dca616f84101439b52c9efea36b3dd6db92af
VadymMakohon <errorks7@gmail.com>
2024-09-23 16:35:39 +0200

    adding link to the js file

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const serchForm = document.getElementById("search-form");
 const serchBox = document.getElementById("search-box");
 const serchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+    keyword = searchBox.value;
+    canst url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}';
+}
