cc76578dc25171cf5d4f0c5c494362922411c823
VadymMakohon <errorks7@gmail.com>
2024-09-23 16:16:38 +0200

    created js file

---
+++ script.js
@@ -0,0 +1,4 @@
+const serchForm = document.getElementById("search-form");
+const serchBox = document.getElementById("search-box");
+const serchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
